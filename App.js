22650e12b43272bcb8ea96b05d28eaf95b4397b5
jaeryo2357 <jaeryo2357@naver.com>
2025-12-21 15:49:38 +0900

    navigation icon 추가 및 item 클릭 가능하도록 수정

--- App.js
+++ App.js
@@ -21,6 +21,16 @@ function ExpensesOverview() {
         headerTintColor: 'white',
         tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
         tabBarActiveTintColor: GlobalStyles.colors.accent500,
+        headerRight: ({ tintColor }) => (
+          <IconButton
+            icon="add"
+            size={24}
+            color={tintColor}
+            onPress={() => {
+              navigation.navigate('ManageExpense');
+            }}
+          />
+        ),
       }}
     >
       <BottomTabs.Screen
